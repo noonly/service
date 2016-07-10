9c6ff08312eba9c4576b3356424af73601e16258
xiaole <x@noonly.com>
2016-07-10 21:02:02 +0800

    add start script run

--- run.sh
+++ run.sh
@@ -35,39 +35,67 @@ do
 done
 path=$tmp
 tmp=""
+#if [ -e conf.txt -a ! -s conf.txt ];then echo "empty"; fi
+#i=0
 
-i=0
 for folder in `ls $path`
 do
 	if [ -d $path"/"$folder"/WebRoot" ]; then
 	
-		if [ -f $path"/"$folder"/WebRoot/temp.ctml" ]; then
-			if [ -f $path"/"$folder"/WebRoot/setvice.json" ]; then
-				continue
-			fi
-		fi
-		read -p "$folder is your project[y/n]?" is
-		if [ "_$is" == "_y" ]; then
-			is=""
-			project=$path"/"$folder"/WebRoot"				
-			#projectname=$folder
-			while [ "_$pname" == "_" ]
+		#if [ -f $path"/"$folder"/WebRoot/temp.ctml" ]; then
+		#	if [ -f $path"/"$folder"/WebRoot/setvice.json" ]; then
+		#		continue
+		#	fi
+		#fi
+		
+		#yy="y"
+		echo "y" > ./data/.local
+		if [ ! -f $path"/"$folder"/WebRoot/temp.ctml" ] || [ ! -f $path"/"$folder"/WebRoot/service.json" ]; then
+			read -p "The path '$folder' is your project[y/n]?" yy
+			echo $yy > ./data/.local
+		else
+			
+			#echo 'yes="y"' >> $HOME/.profile
+			cat ./conf.txt | while read line
 			do
-				read -p "Enter your project name (important!!!):" pname
-				echo "{{range service \"$pname\"}}{{if (.Tags.Contains \"$y\")}}ok{{end}}{{end}}" > "$project/temp.ctml"
+				keyword=`echo $line | awk '{print $2}'`
+				if [ "_$keyword" != "_" ]; then
+					m=`grep $keyword $path"/"$folder"/WebRoot/service.json"`
+					if [ "_$m" != "_" ];then
+						echo "no" > ./data/.local
+					fi
+				fi
 				
 			done
-			while [ "_$ppath" == "_" ]
-			do
-				read -p "Enter your project path (important!!! Access this path must return 200 ok. like:/Login/web):" ppath
-			done
-			#cat ./conf.txt | sed -e '/^$/d' > ./conf.txt
-			echo "$pname $ppath" >> ./conf.txt
-			echo "{\"service\": {\"name\": \"$pname\", \"tags\": [\"web\",\"slave\"], \"port\": $port, \"check\":{\"name\":\"status\",\"http\":\"http://localhost:8080$ppath\",\"interval\":\"30s\"}}}"  > "$project/setvice.json"
-			pname=""
-			ppath=""
-			i=$(($i+1))
 		fi
+		yy=`cat ./data/.local 2> /dev/null`
+		
+		if [ "_$yy" != "_y" ]; then
+			continue
+		fi
+		project=$path"/"$folder"/WebRoot"				
+		#projectname=$folder
+		while [ "_$pname" == "_" ]
+		do
+			read -p "Enter your '$folder' project name (important!!!):" pname
+			echo "{{range service \"$pname\"}}{{if (.Tags.Contains \"$y\")}}ok{{end}}{{end}}" > "$project/temp.ctml"
+			
+		done
+		while [ "_$ppath" == "_" ]
+		do
+			read -p "Enter your '$folder' project path (important!!! Access this path must return 200 ok. like:/Login/web):" ppath
+		done
+		#cat ./conf.txt | sed -e '/^$/d' > ./conf.txt
+		echo -e "$pname $ppath" >> ./conf.txt
+		echo "{\"service\": {\"name\": \"$pname\", \"tags\": [\"web\",\"slave\"], \"port\": $port, \"check\":{\"name\":\"status\",\"http\":\"http://localhost:8080$ppath\",\"interval\":\"30s\"}}}"  > "$project/service.json"
+		pname=""
+		ppath=""
+			
+		#read -p "$folder is your project[y/n]?" is
+		#if [ "_$is" == "_y" ]; then
+		#	is=""
+			
+		#fi
 	fi
 done
 
